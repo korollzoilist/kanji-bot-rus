0a782ed9c22b27ab5b7ceb7d3f07e0c0f48433a4
korollzoilist <g4763862@gmail.com>
2025-12-11 19:43:51 +0500

    The first working webhook application!

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import logging
 import os
 import sys
 import asyncio
+from contextlib import asynccontextmanager
 from dotenv import load_dotenv
 from kanji import Kanji
 from aiogram import Bot, Dispatcher, types, F, Router
@@ -11,9 +12,12 @@ from aiogram.fsm.storage.memory import MemoryStorage
 from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.types.input_file import FSInputFile
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler
 from aiogram.filters import Command, CommandStart
+from fastapi import FastAPI, Request
 
 
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 load_dotenv()
 AIOGRAM_API_TOKEN = os.getenv("AIOGRAM_API_TOKEN")
 
@@ -22,6 +26,28 @@ storage = MemoryStorage()
 #dp = Dispatcher(bot, storage=storage)
 dp = Dispatcher()
 router = Router()
+dp.include_router(router)
+
+WEBHOOK_PATH = "/webhook"
+WEBHOOK_URL = f"https://your-domain.com{WEBHOOK_PATH}"
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await bot.set_webhook(WEBHOOK_URL)
+    logging.info(f"{bot} webhook set")
+
+    yield
+
+    await bot.delete_webhook()
+    logging.info(f"{bot} webhook deleted")
+
+app = FastAPI(lifespan=lifespan)
+
+@app.post("/webhook")
+async def telegram_webhook(request: Request):
+    data = await request.json()
+    await dp.feed_webhook_update(bot, data)
+    return {"ok": True}
 
 
 class KanjiSearch(StatesGroup):
@@ -186,16 +212,3 @@ async def daite_tank(message: types.Message):
 @router.message()
 async def echo(message: types.Message):
     await message.answer(escape_markdown(message.text))
-
-
-async def main():
-    
-    bot = Bot(token=AIOGRAM_API_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN_V2))
-    dp = Dispatcher()
-    dp.include_router(router)
-
-    await dp.start_polling(bot)
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
-    asyncio.run(main())
